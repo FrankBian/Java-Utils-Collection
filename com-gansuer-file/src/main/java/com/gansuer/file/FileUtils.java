ca1437b1592b167629fec6b4fa57028cf1b62cc6
Frank Bian <FrankBian@126.com>
2015-04-28 21:52:04 +0800

    finish the FileUtils.java

--- com-gansuer-file/src/main/java/com/gansuer/file/FileUtils.java
+++ com-gansuer-file/src/main/java/com/gansuer/file/FileUtils.java
@@ -1,8 +1,10 @@
 package com.gansuer.file;
 
+
 import com.gansuer.debug.Debug;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,6 +15,8 @@ import java.util.List;
 public class FileUtils {
 
     private static Debug debug = new Debug();
+
+    private static final int BUFFER_LENGTH = 4096 ;
 
     /**
      * 创建目录
@@ -62,13 +66,6 @@ public class FileUtils {
         }
     }
 
-    /**
-     * 删除文件加
-     * @param folderPath 文件夹路径
-     */
-    public static void delFolder (String folderPath){
-
-    }
 
     /**
      * 删除文件
@@ -76,13 +73,61 @@ public class FileUtils {
      */
     public static void delFile (String fileName) {
 
+        try{
+            File delfile = new File(fileName);
+            delfile.delete();
+        }catch (Exception e){
+            debug.logError("delete file error : " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     /**
-     * 删除文件夹中得所有文件
+     * 删除文件夹
+     * @param folderPath 文件夹路径
+     */
+    public static void delFolder (String folderPath){
+
+        try{
+            delAllFile(folderPath);
+            File folder = new File(folderPath);
+            //删除空文件夹
+            folder.delete();
+        }catch (Exception e){
+            debug.logError("delete folder error : " +e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * 删除文件夹中的所有文件
      * @param path 文件夹路径
      */
     public static void delAllFile (String path){
+        File file = new File(path);
+        if (!file.exists() || !file.isDirectory()){
+            return;
+        }
+
+        String[] children = file.list();
+        File temp = null ;
+        for (String child : children){
+            if (path.endsWith(File.separator)){
+                temp = new File(path + child);
+            }else{
+                temp = new File(path + File.separator + child);
+            }
+
+            if (temp.isFile()){
+                temp.delete();
+            }
+            if (temp.isDirectory()){
+                delAllFile(temp.getPath()); // 删除文件夹下所有的文件
+                delFolder(temp.getPath());  // 删除空文件夹
+            }
+        }
 
     }
 
@@ -92,6 +137,41 @@ public class FileUtils {
      * @param dirFile 目标文件目录； 若文件目录不存在则自动创建
      */
     public static void copyFile (String srcFile , String dirFile) {
+        FileInputStream fis = null ;
+        FileOutputStream fos = null ;
+        try{
+            fis = new FileInputStream(srcFile);
+            mkdir(dirFile);
+            fos = new FileOutputStream(new File(dirFile + File.separator + new File(srcFile).getName()));
+            int len = 0;
+            byte[] buffer = new byte[BUFFER_LENGTH];
+            while((len = fis.read(buffer)) != -1){
+                fos.write(buffer,0,len);
+            }
+            fos.flush();
+            fos.close();
+            fis.close();
+        } catch (Exception e) {
+            debug.logError("Copy file error : " + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    debug.logError("File Stream Error : " +e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    debug.logError("File Stream Error : " +e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        }
 
     }
 
@@ -101,7 +181,53 @@ public class FileUtils {
      * @param dirFolder string 目标文件夹路径
      */
     public static void copyFolder (String srcFolder , String dirFolder) {
+        try{
+            mkdir(dirFolder);
 
+            File file = new File(srcFolder) ;
+            if (!file.exists()){
+                debug.logError("Copy folder error : source folder is not existed ! "  );
+                return ;
+            }
+            String[] children = file.list();
+            if (dirFolder.endsWith(File.separator)){
+                dirFolder = dirFolder.substring(0,dirFolder.length() -1);
+            }
+            File temp = null ;
+
+            FileInputStream fis = null;
+            FileOutputStream fos = null;
+            byte[] buffer = null ;
+            for(String child : children ){
+                if (srcFolder.endsWith(File.separator)){
+                    temp = new File(srcFolder + child);
+                }else{
+                    temp = new File(srcFolder + File.separator + child);
+                }
+
+                if (temp.isFile()){
+                    fis = new FileInputStream(temp);
+                    fos = new FileOutputStream(dirFolder + File.separator + child);
+                    buffer = new byte[BUFFER_LENGTH];
+                    int len = 0;
+                    while ( (len = fis.read(buffer)) != -1){
+                        fos.write(buffer,0,len);
+                    }
+                    fos.flush();
+                    fos.close();
+                    fis.close();
+                }
+
+                if (temp.isDirectory()){
+                    copyFolder(temp.getPath() , dirFolder + File.separator + child);
+                }
+
+            }
+
+        }catch (Exception e){
+            debug.logError("Copy folder error : " +e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -110,7 +236,8 @@ public class FileUtils {
      * @param newPath 目标文件目录
      */
     public static void moveFile (String oldPath ,String newPath){
-
+        copyFile(oldPath,newPath);
+        delFile(oldPath);
     }
 
     /**
@@ -119,7 +246,8 @@ public class FileUtils {
      * @param newPath 目标文件目录
      */
     public static void moveFiles (String oldPath , String newPath){
-
+        copyFolder(oldPath,newPath);
+        delAllFile(oldPath);
     }
     /**
      * 移动文件到指定目录，会删除文件夹
@@ -127,7 +255,8 @@ public class FileUtils {
      * @param newPath 目标文件目录
      */
     public static void moveFolder (String oldPath ,String newPath){
-
+        copyFolder(oldPath,newPath);
+        delFolder(oldPath);
     }
 
     /**
@@ -158,7 +287,17 @@ public class FileUtils {
      * @throws Exception
      */
     public static String readData(InputStream inputStream , String charsetName ) throws Exception {
-        return null;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[BUFFER_LENGTH];
+        int len = -1;
+        while ((len = inputStream.read(buffer)) != -1){
+            byteArrayOutputStream.write(buffer, 0 ,len);
+        }
+
+        byte[] data = byteArrayOutputStream.toByteArray();
+        byteArrayOutputStream.close();
+        inputStream.close();
+        return new String(data,charsetName);
     }
 
     /**
@@ -168,7 +307,16 @@ public class FileUtils {
      * @throws Exception
      */
     public static List<String> readFile(String path) throws Exception {
-        return null;
+
+        List<String> lines = new ArrayList<String>();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String line = reader.readLine();
+        while ( line != null){
+            lines.add(line);
+            line = reader.readLine() ;
+        }
+        reader.close();
+        return lines;
     }
 
 
