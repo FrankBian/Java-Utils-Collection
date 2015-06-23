aa1f50c91f222d13e01aa75c57532304dc48ed7d
Frank Bian <FrankBian@126.com>
2015-06-23 15:26:29 +0800

    update the leetcode-module

---
+++ com-gansuer-leetcode/Problem.md
@@ -0,0 +1,25 @@
+
+# Intro
+
+This below problems are from [the LeetCode OJ](https://leetcode.com/problems/)
+
+## 1. Two Sum
+
+this is the description :
+
+>Given an array of integers, find two numbers such that they add up to a specific target number.  
+>The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.  
+>You may assume that each input would have exactly one solution.    
+>Input: numbers={2, 7, 11, 15}, target=9   
+>Output: index1=1, index2=2   
+
+## 2. Add Two Numbers
+
+this is the description :
+
+>You are given two linked lists representing two non-negative numbers. 
+>The digits are stored in reverse order and each of their nodes contain a single digit.    
+>Add the two numbers and return it as a linked list.   
+>Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)   
+>Output: 7 -> 0 -> 8   
+
---
+++ com-gansuer-leetcode/src/main/java/Solution2.java
@@ -0,0 +1,82 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null){
+            return null;
+        }
+
+        int tmp = (l1.val + l2.val)/10 , mod = (l1.val + l2.val)%10 ;
+        ListNode head = new ListNode(mod), next = null;
+        head.next = next;
+
+        l1 = l1.next ;
+        l2 = l2.next ;
+        while (l1 != null && l2 != null){
+            mod = (l1.val + l2.val)%10 ;
+            next = new ListNode(mod +tmp);
+            tmp = (l1.val + l2.val)/10 ;
+            next = next.next;
+            l1 = l1.next ;
+            l2 = l2.next ;
+        }
+        if (l1 == null && l2 == null && tmp ==0){
+            next = null;
+        }else if (l1 == null && l2 ==null && tmp > 0){
+            next = new ListNode(tmp);
+            next.next = null;
+        }else if (l1 == null && l2 != null){
+            next = l2;
+        }else if (l1 != null && l2 == null){
+            next = l1;
+        }
+        return head;
+    }
+
+    public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null){
+            return null;
+        }
+
+        int tmp = (l1.val + l2.val)/10 , mod = (l1.val + l2.val)%10 ;
+        ListNode head = new ListNode(mod), next = null;
+        head.next = next;
+
+        l1 = l1.next ;
+        l2 = l2.next ;
+        while (l1 != null && l2 != null){
+            mod = (l1.val + l2.val)%10 ;
+            next = new ListNode(mod +tmp);
+            tmp = (l1.val + l2.val)/10 ;
+            next = next.next;
+            l1 = l1.next ;
+            l2 = l2.next ;
+        }
+        if (l1 == null && l2 == null && tmp ==0){
+            next = null;
+        }else if (l1 == null && l2 ==null && tmp > 0){
+            next = new ListNode(tmp);
+            next.next = null;
+        }else if (l1 == null && l2 != null){
+            next = l2;
+        }else if (l1 != null && l2 == null){
+            next = l1;
+        }
+
+        return head;
+    }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x){
+            val = x;
+        }
+    }
+}
