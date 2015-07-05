8c868f8f10b321ed28ceb9c369336d6e236d2115
Frank Bian <FrankBian@126.com>
2015-07-05 19:09:44 +0800

    update

--- com-gansuer-leetcode/src/main/java/com/gansuer/leetcode/Solution3.java
+++ com-gansuer-leetcode/src/main/java/com/gansuer/leetcode/Solution3.java
@@ -1,5 +1,8 @@
 package com.gansuer.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Frank on 7/5/15.
  */
@@ -8,21 +11,22 @@ public class Solution3 {
     /**
      * O(n^2)
      * Last executed error : Time Limit Exceeded
+     *
      * @param s
      * @return
      */
     public int lengthOfLongestSubstringV1(String s) {
-        if (s == null || s.length() == 0){
+        if (s == null || s.length() == 0) {
             return 0;
         }
         int max = 0;
         char[] strings = s.toCharArray();
-        for(int i = 0; i < strings.length ; i++){
-            String tmp = strings[i]+"";
-            for (int j = i+1; j < strings.length; j++){
-                if (tmp.indexOf(strings[j]) < 0){ // No Repeats
+        for (int i = 0; i < strings.length; i++) {
+            String tmp = strings[i] + "";
+            for (int j = i + 1; j < strings.length; j++) {
+                if (tmp.indexOf(strings[j]) < 0) { // No Repeats
                     tmp += strings[j];
-                }else {
+                } else {
                     break;
                 }
             }
@@ -37,25 +41,25 @@ public class Solution3 {
      * @param s
      * @return
      */
-    public int lengthOfLongestSubstringV2(String s){
+    public int lengthOfLongestSubstringV2(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
 
-        int max = 0 , begin = 0 , end = 0;
+        int max = 0, begin = 0, end = 0;
         String temp = "";
         char[] chars = s.toCharArray();
-        while ( end < chars.length){
-            if (temp.indexOf(chars[end]) < 0){
+        while (end < chars.length) {
+            if (temp.indexOf(chars[end]) < 0) {
                 temp += chars[end];
                 end++;
-            }else{
-                max = temp.length() > max ? temp.length() : max ;
+            } else {
+                max = temp.length() > max ? temp.length() : max;
                 end = ++begin;
                 temp = "";
             }
         }
-        max = temp.length() > max ? temp.length() : max ;
+        max = temp.length() > max ? temp.length() : max;
         return max;
     }
 
@@ -63,8 +67,9 @@ public class Solution3 {
      * accepted
      * 981/981 test cases passed
      * 444ms
-     *
+     * <p/>
      * this solution from discuss
+     *
      * @param s
      * @return
      */
@@ -74,14 +79,13 @@ public class Solution3 {
             for (; j <= s.length(); j++) {
                 if (i == j) continue;
                 String sub = s.substring(i, j);
-                if (sub.indexOf(s.charAt(j-1)) == sub.length() - 1) {
+                if (sub.indexOf(s.charAt(j - 1)) == sub.length() - 1) {
                     // enhancement by me
-                    max = j-i > max ? j-i : max;
+                    max = j - i > max ? j - i : max;
 //                    if (max < j-i){
 //                        max = j-i;
 //                    }
-                }
-                else {
+                } else {
                     break;
                 }
             }
@@ -93,24 +97,25 @@ public class Solution3 {
      * accepted
      * 981/981 test cases passed
      * 436ms
+     *
      * @param s
      * @return
      */
-    public int lengthOfLongestSubstringV4(String s){
+    public int lengthOfLongestSubstringV4(String s) {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        int max = 0 , begin = 0 , end = 0;
-        while (end < s.length()+1){
-            if (begin == end){
+        int max = 0, begin = 0, end = 0;
+        while (end < s.length() + 1) {
+            if (begin == end) {
                 end++;
                 continue;
             }
-            String sub = s.substring(begin,end);
-            if (sub.indexOf(s.charAt(end-1)) == sub.length() - 1){
-                max = (end-begin) > max ? (end-begin) : max;
+            String sub = s.substring(begin, end);
+            if (sub.indexOf(s.charAt(end - 1)) == sub.length() - 1) {
+                max = (end - begin) > max ? (end - begin) : max;
                 end++;
-            }else{
+            } else {
                 begin++;
             }
         }
@@ -120,8 +125,9 @@ public class Solution3 {
 
     /**
      * From discuss of leetcode
-     *
+     * <p/>
      * max of length of substring between the same characters
+     *
      * @param s
      * @return
      */
@@ -141,6 +147,56 @@ public class Solution3 {
                 m[ch[i]] = i;
             }
             max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+
+
+    /**
+     * Time Limit Exceeded
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringV6(String s) {
+        if (s == null) return 0;
+        if (s.length() < 2) return s.length();
+        Map<Character, Integer> hash = new HashMap();
+        int length = s.length();
+        int max = 0, end = 0, start = 0;
+        for (;end < length; end++ ){
+            if (hash.containsKey(s.charAt(end)) && hash.get(s.charAt(end)) >= start){
+                start = hash.get(s.charAt(end)) + 1;
+            }
+            hash.put(s.charAt(end),end);
+            max = Math.max(max , end-start+1);
+        }
+        return max;
+    }
+
+    /**
+     * from discuss
+     * 981/981 test cases passed
+     * Runtime : 392ms
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstringV7(String s){
+        if(s.length()<2) return s.length();
+        Map <Character, Integer> hash = new HashMap();
+        int length=s.length();
+        int max=1,end = 1,start = 0;
+
+        hash.put(s.charAt(0),0);
+        while(end<length){
+            if(hash.containsKey(s.charAt(end))&&hash.get(s.charAt(end))>=start){
+                //s.charAt(end))>=start keep start not backforward
+                //or abba＝3
+                start = hash.get(s.charAt(end))+1;
+            }
+            max = Math.max(max,end-start+1);
+            hash.put(s.charAt(end),end);
+            end++;
         }
         return max;
     }
--- com-gansuer-leetcode/src/test/java/com/gansuer/leetcode/Solution3Test.java
+++ com-gansuer-leetcode/src/test/java/com/gansuer/leetcode/Solution3Test.java
@@ -97,4 +97,26 @@ public class Solution3Test {
         Assert.assertEquals(2,res5);
     }
 
+    @Test
+    public void testLengthOfLongestSubstringV6() throws Exception {
+        int res = solution3.lengthOfLongestSubstringV6("abcabcbb");
+        int res1 = solution3.lengthOfLongestSubstringV6("bbbbbb");
+        Date begin = new Date();
+        int res2 = solution3.lengthOfLongestSubstringV6(cases);
+        Date end = new Date();
+        int res3 = solution3.lengthOfLongestSubstringV6("c");
+        int res4 = solution3.lengthOfLongestSubstringV6("pwwkew");
+        int res5 = solution3.lengthOfLongestSubstringV6("au");
+
+        System.out.println("length of the target String : " + cases.length());
+        System.out.println("length of longest Substring : " + res2);
+        System.out.println("Times : " + (end.getTime()-begin.getTime()));
+        Assert.assertEquals(3,res);
+        Assert.assertEquals(1,res1);
+        Assert.assertEquals(95,res2);
+        Assert.assertEquals(1,res3);
+        Assert.assertEquals(3,res4);
+        Assert.assertEquals(2,res5);
+    }
+
 }
