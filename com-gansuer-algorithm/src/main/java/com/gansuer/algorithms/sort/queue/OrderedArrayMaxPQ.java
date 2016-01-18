abdf6061b11652f0605c84b9869b4229e96f9388
Frank Bian <FrankBian@126.com>
2016-01-18 22:25:51 +0800

    append PriorityQueue instance

---
+++ com-gansuer-algorithm/src/main/java/com/gansuer/algorithms/sort/queue/OrderedArrayMaxPQ.java
@@ -0,0 +1,61 @@
+package com.gansuer.algorithms.sort.queue;
+
+/**
+ * Created by Frank on 1/14/2016.
+ */
+public class OrderedArrayMaxPQ<K extends Comparable<K>> extends UnorderArrayMaxPQ {
+
+    public OrderedArrayMaxPQ() {
+        super();
+    }
+
+    public OrderedArrayMaxPQ(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public Comparable delMax() {
+        if (isEmpty()) return null;
+        Comparable res = values[--size];
+        values[size] = null;
+        return res;
+    }
+
+    /**
+     * the array is AES ordered
+     *
+     * @param key
+     */
+    @Override
+    public void insert(Comparable key) {
+        int index = 0;
+        for (; index < size; index++) {
+            if (values[index].compareTo(key) > 0) {
+                break;
+            }
+        }
+        if (size < values.length) {
+            if (index == size) {
+                values[size++] = key;
+                return;
+            }
+            for (int i = size - 1; i > index; i--) {
+                values[i + 1] = values[i];
+            }
+            values[index] = key;
+            size++;
+            return;
+        }
+        // the array is full of element
+        for (int i = 1; i < index; i++) {
+            values[i - 1] = values[i];
+        }
+        values[index - 1] = key;
+    }
+
+    @Override
+    public Comparable max() {
+        if (isEmpty()) return null;
+        return values[size-1];
+    }
+}
---
+++ com-gansuer-algorithm/src/main/java/com/gansuer/algorithms/sort/queue/UnorderArrayMaxPQ.java
@@ -0,0 +1,76 @@
+package com.gansuer.algorithms.sort.queue;
+
+/**
+ * Created by Frank on 1/13/2016.
+ */
+public class UnorderArrayMaxPQ<K extends Comparable<K>> implements MaxPQ<K> {
+
+    protected K[] values;
+    protected int size;
+    protected static final int DEFAULT_CAPACITY = 16;
+
+    public UnorderArrayMaxPQ() {
+        values = (K[]) new Comparable[DEFAULT_CAPACITY];
+    }
+
+    public UnorderArrayMaxPQ(int capacity) {
+        values = (K[]) new Comparable[capacity];
+    }
+
+    @Override
+    public K max() {
+        if (isEmpty()) return null;
+        K max = values[0];
+        for (int i = 1; i < size; i++) {
+            if (values[i].compareTo(max) > 0) {
+                max = values[i];
+            }
+        }
+        return max;
+    }
+
+    @Override
+    public K delMax() {
+        if (isEmpty()) return null;
+        int index = 0;
+        for (int i = 1; i < size; i++) {
+            if (values[i].compareTo(values[index]) > 0) {
+                index = i;
+            }
+        }
+        K res = values[index];
+        values[index] = null;
+        for (int i = index; i < size - 1; i++) {
+            values[index] = values[index + 1];
+        }
+        size--;
+        return res;
+    }
+
+    @Override
+    public void insert(K key) {
+        if (size < values.length) {
+            values[size++] = key;
+        } else {
+            K min = values[0];
+            int index = 0;
+            for (int i = 1; i < size; i++) {
+                if (min.compareTo(values[i]) > 0) {
+                    index = i;
+                }
+            }
+            values[index] = key;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
