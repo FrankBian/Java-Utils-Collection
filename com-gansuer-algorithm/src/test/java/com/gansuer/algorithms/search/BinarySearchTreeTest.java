e95b59413b22174b0aa83f8b0cdf50d665353550
Frank Bian <FrankBian@126.com>
2016-03-03 21:14:56 +0800

    add junit-test

---
+++ com-gansuer-algorithm/src/test/java/com/gansuer/algorithms/search/BinarySearchTreeTest.java
@@ -0,0 +1,97 @@
+package com.gansuer.algorithms.search;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Frank on 3/3/2016.
+ */
+public class BinarySearchTreeTest {
+
+    private BinarySearchTree<Integer, String> bst;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testMin() throws Exception {
+
+    }
+
+    @Test
+    public void testMax() throws Exception {
+
+    }
+
+    @Test
+    public void testFloor() throws Exception {
+
+    }
+
+    @Test
+    public void testCeiling() throws Exception {
+
+    }
+
+    @Test
+    public void testRank() throws Exception {
+
+    }
+
+    @Test
+    public void testSelect() throws Exception {
+
+    }
+
+    @Test
+    public void testKeys() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteMin() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteMax() throws Exception {
+
+    }
+
+    @Test
+    public void testPut() throws Exception {
+
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testContains() throws Exception {
+
+    }
+
+    @Test
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    @Test
+    public void testSize() throws Exception {
+
+    }
+
+    @Test
+    public void testKeys1() throws Exception {
+
+    }
+}
