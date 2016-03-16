25f4c5668ef854893a0d67d36d3879acd73ca639
Frank Bian <FrankBian@126.com>
2016-03-16 20:39:01 +0800

    update

--- com-gansuer-leetcode/src/main/java/com/gansuer/leetcode/array/Solution189.java
+++ com-gansuer-leetcode/src/main/java/com/gansuer/leetcode/array/Solution189.java
@@ -9,13 +9,13 @@ public class Solution189 {
         if (nums == null) return;
         k = k % nums.length;
         if (k == 0) return;
-//        rotate(nums, 0, nums.length, k, 1);
-        rotateRight(nums, 0, nums.length, k);
+        rotate(nums, 0, nums.length, k, 1);
+//        rotateRight(nums, 0, nums.length, k);
     }
 
     /**
-     * Time Limit Exceeded
-     *
+     * AC
+     * 11.70%
      * @param nums
      * @param from
      * @param to
