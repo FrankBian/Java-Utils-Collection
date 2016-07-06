dbac512a03eb670d97fd70a7b4dbca8a5e231445
Frank Bian <FrankBian@126.com>
2016-07-06 16:30:24 +0800

    update; append StrongConnectedComponent

---
+++ com-gansuer-algorithm/src/main/java/com/gansuer/algorithms/graphs/StrongConnectedComponent.java
@@ -0,0 +1,62 @@
+package com.gansuer.algorithms.graphs;
+
+/**
+ * Created by Frank on 7/5/16.
+ */
+public class StrongConnectedComponent {
+
+    private boolean[] marked;
+    private int count;
+    private int[] ids;
+
+    public StrongConnectedComponent(Digraph digraph) {
+        if (digraph == null) throw new IllegalArgumentException("graph is null");
+        this.marked = new boolean[digraph.getVertices()];
+        this.ids = new int[digraph.getVertices()];
+        DepthFirstOrder depthFirstOrder = new DepthFirstOrder(digraph.reverse());
+        for (int v : depthFirstOrder.reversePost()) {
+            if (!marked[v]) {
+                dfs(digraph, v);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Digraph digraph, int v) {
+        marked[v] = true;
+        ids[v] = count;
+        for (int w : digraph.adj(v)) {
+            if (!marked[w]) dfs(digraph, w);
+        }
+    }
+
+    /**
+     * Are v and w strongly connected
+     *
+     * @param v
+     * @param w
+     * @return
+     */
+    public boolean isStronglyConnected(int v, int w) {
+        return ids[v] == ids[w];
+    }
+
+    /**
+     * number of strong components
+     *
+     * @return
+     */
+    public int count() {
+        return count;
+    }
+
+    /**
+     * component identifier for v
+     *
+     * @param v
+     * @return
+     */
+    public int getComponentId(int v) {
+        return ids[v];
+    }
+}
---
+++ com-gansuer-algorithm/src/test/java/com/gansuer/algorithms/graphs/StrongConnectedComponentTest.java
@@ -0,0 +1,43 @@
+package com.gansuer.algorithms.graphs;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Frank on 7/6/16.
+ */
+public class StrongConnectedComponentTest extends DigraphTestBase {
+
+    private StrongConnectedComponent scc;
+
+    @Before
+    public void setUp() throws Exception {
+        init(false);
+        scc = new StrongConnectedComponent(digraph);
+    }
+
+    @Test
+    public void isStronglyConnected() throws Exception {
+        Assert.assertTrue(scc.isStronglyConnected(0,2));
+        Assert.assertTrue(scc.isStronglyConnected(0,3));
+        Assert.assertTrue(scc.isStronglyConnected(0,4));
+        Assert.assertTrue(scc.isStronglyConnected(0,5));
+        Assert.assertTrue(!scc.isStronglyConnected(0,1));
+
+
+    }
+
+    @Test
+    public void count() throws Exception {
+        Assert.assertEquals(5,scc.count());
+    }
+
+    @Test
+    public void getComponentId() throws Exception {
+        for (int v = 0 ; v < digraph.getVertices();v++){
+            System.out.println("vertex " + v +" id = " + scc.getComponentId(v));
+        }
+    }
+
+}
