43c7e9cb08289915f3265ac9ff01d00939413c85
Frank Bian <FrankBian@126.com>
2016-07-19 17:45:03 +0800

    update ,add shortestpath

---
+++ com-gansuer-algorithm/src/main/java/com/gansuer/algorithms/graphs/shortestpath/DirectedEdge.java
@@ -0,0 +1,82 @@
+package com.gansuer.algorithms.graphs.shortestpath;
+
+/**
+ * Created by Frank on 7/19/16.
+ */
+public final class DirectedEdge implements Comparable<DirectedEdge> {
+
+    private final int from;
+    private final int to;
+    private final double weight;
+
+    public DirectedEdge(int from, int to, double weight) {
+        if (from < 0)
+            throw new IndexOutOfBoundsException("Vertex name must be a non-negative integer");
+        if (to < 0)
+            throw new IndexOutOfBoundsException("Vertex name must be a non-negative integer");
+        this.from = from;
+        this.to = to;
+        if (Double.isNaN(weight) || weight < 0)
+            throw new IllegalArgumentException("weight must be positive(or zero) :" + weight);
+        this.weight = weight;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    /**
+     * Compares this object with the specified object for order.  Returns a
+     * negative integer, zero, or a positive integer as this object is less
+     * than, equal to, or greater than the specified object.
+     * <p>
+     * <p>The implementor must ensure <tt>sgn(x.compareTo(y)) ==
+     * -sgn(y.compareTo(x))</tt> for all <tt>x</tt> and <tt>y</tt>.  (This
+     * implies that <tt>x.compareTo(y)</tt> must throw an exception iff
+     * <tt>y.compareTo(x)</tt> throws an exception.)
+     * <p>
+     * <p>The implementor must also ensure that the relation is transitive:
+     * <tt>(x.compareTo(y)&gt;0 &amp;&amp; y.compareTo(z)&gt;0)</tt> implies
+     * <tt>x.compareTo(z)&gt;0</tt>.
+     * <p>
+     * <p>Finally, the implementor must ensure that <tt>x.compareTo(y)==0</tt>
+     * implies that <tt>sgn(x.compareTo(z)) == sgn(y.compareTo(z))</tt>, for
+     * all <tt>z</tt>.
+     * <p>
+     * <p>It is strongly recommended, but <i>not</i> strictly required that
+     * <tt>(x.compareTo(y)==0) == (x.equals(y))</tt>.  Generally speaking, any
+     * class that implements the <tt>Comparable</tt> interface and violates
+     * this condition should clearly indicate this fact.  The recommended
+     * language is "Note: this class has a natural ordering that is
+     * inconsistent with equals."
+     * <p>
+     * <p>In the foregoing description, the notation
+     * <tt>sgn(</tt><i>expression</i><tt>)</tt> designates the mathematical
+     * <i>signum</i> function, which is defined to return one of <tt>-1</tt>,
+     * <tt>0</tt>, or <tt>1</tt> according to whether the value of
+     * <i>expression</i> is negative, zero or positive.
+     *
+     * @param that the object to be compared.
+     * @return a negative integer, zero, or a positive integer as this object
+     * is less than, equal to, or greater than the specified object.
+     * @throws NullPointerException if the specified object is null
+     * @throws ClassCastException   if the specified object's type prevents it
+     *                              from being compared to this object.
+     */
+    @Override
+    public int compareTo(DirectedEdge that) {
+        return Double.compare(this.weight, that.getWeight());
+    }
+
+    public String toString() {
+        return from + " -> " + to + "(" + String.format("%5.2f", weight) + ")";
+    }
+}
---
+++ com-gansuer-algorithm/src/main/java/com/gansuer/algorithms/graphs/shortestpath/EdgeWeightDigraph.java
@@ -0,0 +1,60 @@
+package com.gansuer.algorithms.graphs.shortestpath;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Frank on 7/19/16.
+ */
+public class EdgeWeightDigraph {
+
+    private int vertices;
+    private int edges;
+    private Set<DirectedEdge>[] adjs;
+
+    public EdgeWeightDigraph(int vertices) {
+        this.vertices = vertices;
+        this.adjs = new Set[vertices];
+        for (int i = 0; i < vertices; i++) {
+            adjs[i] = new HashSet<>();
+        }
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public int getEdges() {
+        return edges;
+    }
+
+    public boolean addEdge(DirectedEdge edge) {
+        if (edge == null) return false;
+        adjs[edge.getFrom()].add(edge);
+        return true;
+    }
+
+    public Set<DirectedEdge> adj(int v) {
+        return adjs[v];
+    }
+
+    public List<DirectedEdge> getEdgeList() {
+        List<DirectedEdge> edgeList = new ArrayList<>();
+        for (int i = 0; i < adjs.length; i++) {
+            edgeList.addAll(adjs[i]);
+        }
+        return edgeList;
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < vertices; i++) {
+            stringBuilder.append(i).append(" : ");
+            adjs[i].forEach(edge -> stringBuilder.append(edge.toString()).append(" "));
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
